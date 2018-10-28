c59d4670b2de22e272ce4aad42e4e26bbafd8cca
Leon Cheng <57leonardo@gmail.com>
2018-10-28 17:22:01 -0400

    setup folderes for aech member

--- jeremy/app.py
+++ jeremy/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 # Index Page
 @app.route('/')
 def index():
-    pass
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
